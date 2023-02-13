c4fd1cfc25d3e3e29f58228fdb4b6143c1c28301
Davidyoon <davidyoon@koreainvestment.com>
2023-02-14 07:48:26 +0900

    Use the shadow effect
    * Added an imageView
    * Set shadow effect

--- ShadowSample/ViewController.swift
+++ ShadowSample/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var vw: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .cyan
+        imageView.layer.shadowOpacity = 1.0
+        imageView.layer.shadowRadius = 5.0
+        imageView.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
+}
 
+private extension ViewController {
+    func setupViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(vw)
 
+        
+        NSLayoutConstraint.activate([
+            vw.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vw.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            vw.widthAnchor.constraint(equalToConstant: 200),
+            vw.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        vw.layoutIfNeeded()
+        
+        vw.layer.shadowPath = UIBezierPath(rect: vw.bounds).cgPath
+    }
 }
 
